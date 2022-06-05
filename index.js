496066d661158d1f433c43b4cb1acff6ab18495c
vincenbryaniel@gmail.com <vincenbryaniel@gmail.com>
2022-06-05 10:25:59 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,51 @@
+function add() {
+    // we'll fill this in shortly
+  }
+  function subtract() {
+
+  }
+
+function multiply() {
+  // we'll fill this in shortly
+}
+function divide() {
+    // we'll fill this in shortly
+}
+function add() {
+    return "something";
+  }
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+
+  function  multiply(a, b) {
+    return a * b;
+  }
+
+  function  divide(a, b) {
+    return a / b;
+  }
+  
+  function  increment(a) {
+    return a + 1;
+  }
+
+  function  decrement(a) {
+    return a - 1;
+  }
+
+  function  makeInt(a) {
+    return parseInt (a,10);
+  }
+
+  function  preserveDecimal(a) {
+    return parseFloat(a);
+  }
+
+
+  
